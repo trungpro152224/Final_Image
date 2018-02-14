f8f314c774338ab1e10494e8fd3f588d51dc3f9b
TRUNG <trungpro151224@gmail.com>
2018-02-14 14:34:27 +0700

    add username to register

--- Final_Image/app/models/user.rb
+++ Final_Image/app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_uniqueness_of :username
+  
 end
